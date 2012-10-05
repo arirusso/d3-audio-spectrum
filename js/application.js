a8b8d9bd12ef590b80999efb3123b99f456cfe24
Ari Russo <ari.russo@gmail.com>
2012-10-05 13:16:36 -0700

    load url from entry box when available

--- js/application.js
+++ js/application.js
@@ -1,10 +1,41 @@
 function Application() {
-  this.url = "media/sweep.mp3";
+  this.populateUrl("media/sweep.mp3");
   this.audio;
   this.context;
   this.model;
   this.source;
   this.view;
+}
+
+Application.prototype.populateUrl = function(defaultUrl) {
+  var src = this.query().src;
+  if (src != null && src != "") {
+    this.url = "/audio?src=" + this.query().src;
+    document.getElementById("audioUrl").value = unescape(src);
+  } else {
+    this.url = defaultUrl;
+  }
+}
+
+Application.prototype.query = function () {
+  var query_string = {};
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+        // If first entry with this name
+    if (typeof query_string[pair[0]] === "undefined") {
+      query_string[pair[0]] = pair[1];
+        // If second entry with this name
+    } else if (typeof query_string[pair[0]] === "string") {
+      var arr = [ query_string[pair[0]], pair[1] ];
+      query_string[pair[0]] = arr;
+        // If third or later entry with this name
+    } else {
+      query_string[pair[0]].push(pair[1]);
+    }
+  } 
+  return query_string;
 }
 
 Application.prototype.load = function() {
