03ebaaf026696da556222911031f1d31fa69c72d
Ari Russo <ari.russo@gmail.com>
2016-11-21 16:52:36 -0800

    naming, visibility

--- public/js/analysis/model.js
+++ public/js/analysis/model.js
@@ -2,43 +2,48 @@
   This is where the audio data is modeled for analysis
 */
 SA.Analysis.Model = function(audio) {
-  this.audio = audio;
-  this.analysis = this.audio.context.createScriptProcessor(this.audio.bufferSize);
-  this.curve = 8;
-  this.intensity = 50;
+  this._data;
+  this._delta;
+  this._fft;
+  this._resolution;
+
+  this._audio = audio;
+  this._analysis = this._audio.context.createScriptProcessor(this._audio.bufferSize);
+  this._curve = 8;
+  this._intensity = 50;
   this.setResolution(1);
 }
 
 SA.Analysis.Model.prototype.setResolution = function(n) {
-  this.resolution = this._linLog(this.audio.bufferSize / n);
+  this._resolution = this._linLog(this._audio.bufferSize / n);
   this.reset();
 }
 
 SA.Analysis.Model.prototype.setCurve = function(n) {
-  this.curve = n;
+  this._curve = n;
   this.reset();
 }
 
 SA.Analysis.Model.prototype.reset = function() {
-  this.data = [];
-  this.delta = [];
-  var fftSize = this.resolution;
-  this.audio.mono = new Float32Array(fftSize);
-  this.fft = new FFT(fftSize, this.audio.sampleRate);
+  this._data = [];
+  this._delta = [];
+  var fftSize = this._resolution;
+  this._audio.mono = new Float32Array(fftSize);
+  this._fft = new FFT(fftSize, this._audio.sampleRate);
   var analyzer = this;
-  this.analysis.onaudioprocess = function(event) {
+  this._analysis.onaudioprocess = function(event) {
     analyzer._audioReceived(event);
   };
 }
 
 SA.Analysis.Model.prototype.length = function() {
-  return this.fft.spectrum.length/2;
+  return this._fft.spectrum.length/2;
 }
 
 SA.Analysis.Model.prototype.play = function(callback) {
   var analyzer = this;
-  this.audio.play(function() {
-    analyzer.audio.connectProcessor(analyzer.analysis);
+  this._audio.play(function() {
+    analyzer._audio.connectProcessor(analyzer.analysis);
     callback();
   });
 }
@@ -59,14 +64,14 @@ SA.Analysis.Model.prototype._linLog = function(n) {
   Execute the given callback using the current analyzer response curve
 */
 SA.Analysis.Model.prototype._withCurve = function(callback) {
-  var segmentLength = this.length() / this.curve;
+  var segmentLength = this.length() / this._curve;
   var segmentCounter = 0;
   var segment = 0;
   var counter = 0;
   var index = 0;
   while (index <= this.length() - 1) {
     callback(index, counter);
-    index += (segment * this.curve) + 1;
+    index += (segment * this._curve) + 1;
     counter += 1;
     segmentCounter += 1;
     if (segmentCounter > segmentLength - 1) {
@@ -80,17 +85,17 @@ SA.Analysis.Model.prototype._withCurve = function(callback) {
   Populate the graph data using the current audio frames
 */
 SA.Analysis.Model.prototype._populateData = function(index, counter) {
-  amplitude = this.fft.spectrum[index] * (this.intensity * 200);
-  this.delta[counter] = amplitude - this.data[counter];
-  this.data[counter] = amplitude;
+  amplitude = this._fft.spectrum[index] * (this._intensity * 200);
+  this._delta[counter] = amplitude - this._data[counter];
+  this._data[counter] = amplitude;
 }
 
 /*
   Method that is called when a new audio frame is available
 */
 SA.Analysis.Model.prototype._audioReceived = function(event) {
-  this.audio.routeAudio(event);
-  this.fft.forward(this.audio.mono);
+  this._audio.routeAudio(event);
+  this._fft.forward(this._audio.mono);
 
   var analyzer = this;
   this._withCurve(function(index, counter) {
